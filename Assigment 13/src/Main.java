14f326d91029ddc43b190e9451734800f194365f
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2022-03-22 14:41:11 -0400

    [Assigment13]

---
+++ Assigment 13/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayGenerator arr1 = new ArrayGenerator();
+        System.out.println(arr1);
+    }
+}
